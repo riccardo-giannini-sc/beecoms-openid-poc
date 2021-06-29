77fa4a36882802b68fc777b948d7b2a9b55f8a3a
Matteo Trentin <matteo.trentin@studio.unibo.it>
2021-06-29 16:34:50 +0200

    First oauth app (django, prm)

--- mysql/init/01-databases.sql
+++
@@ -1,8 +0,0 @@
-# create databases
-CREATE DATABASE IF NOT EXISTS `layer`;
-CREATE DATABASE IF NOT EXISTS `prm`;
-CREATE DATABASE IF NOT EXISTS `serverapp`;
-
-# grant privileges on these databases to mysql user
-CREATE USER 'laravel'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'laravel'@'%';
